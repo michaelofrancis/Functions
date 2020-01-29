1ef22df8f2f61b585596eaff91c2e50f83c918ce
Mike Francis <51752343+michaelofrancis@users.noreply.github.com>
2020-01-29 11:46:31 -0500

    Update and rename manyColsToDummy to manyColsToDummy.R

---
+++ manyColsToDummy.R
@@ -0,0 +1,29 @@
+#A base-R script that searches over many input columns for input values 
+#and returns a dataframe of dummy variables that indicate if the values 
+#were present in these columns.
+
+
+manyColsToDummy<-function(search_terms, search_columns,
+                          output_table){
+    #initialize output table
+    temp_table<-data.frame(matrix(ncol=length(search_terms),
+                                  nrow= nrow(search_columns)))
+    colnames(temp_table)<-search_terms
+    
+    #make table
+    for (i in 1:length(search_terms)){
+        vec<-rowSums(sapply(search_columns,
+                            function(x) grepl(search_terms[i], x, ignore.case = TRUE)
+        ))>0
+        temp_table[,i]<-vec
+    }
+    temp_table<-sapply(temp_table, as.integer, as.logical)
+    temp_table<-as.data.frame(temp_table)
+    assign(x = output_table, value = temp_table, envir = globalenv())
+}
+
+
+#Example
+#manyColsToDummy("find_this", big_table[,columns_to_look_in], "dummy_output_table")
+
+#Visual example found here: https://ibb.co/VwHvBLT
